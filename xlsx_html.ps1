080daecaf1608a7c16b35b4d9296269b80f70496
Michal Jankowski <jankowskim@emea.cshare.net>
2022-05-05 13:23:03 +0100

    Replace header with shorter names.

--- xlsx_html.ps1
+++ xlsx_html.ps1
@@ -1,21 +1,27 @@
 Function ExcelToCsv ($File) {
     $Excel = New-Object -ComObject Excel.Application
-    $wb = $Excel.Workbooks.Open($File)
+    $Wb = $Excel.Workbooks.Open($File)
         
-    $x = $File | Select-Object Directory, BaseName
-    $n = [System.IO.Path]::Combine($x.Directory, (($x.BaseName, 'csv') -join "."))
+    $X = $File | Select-Object Directory, BaseName
+    $N = [System.IO.Path]::Combine($X.Directory, (($X.BaseName, 'csv') -join "."))
     
-    foreach ($ws in $wb.Worksheets) {
-        $ws.SaveAs($n, 6)
+    foreach ($Ws in $Wb.Worksheets) {
+        $Ws.SaveAs($N, 6)
     }
     $Excel.Quit()
 }
 
 Function CsvToJson ($File) {
-    $x = $File | Select-Object Directory, BaseName
-    $n = [System.IO.Path]::Combine($x.Directory, (($x.BaseName, 'json') -join "."))
+    $X = $File | Select-Object Directory, BaseName
+    $N = [System.IO.Path]::Combine($X.Directory, (($X.BaseName, 'json') -join "."))
 
-    Import-Csv $File | ConvertTo-Json -Compress | Out-File $n
+    $Header = 'A', 'B'
+
+    $O = Get-Content $File
+    $O = $O[1..($O.Count - 1)]
+    $O | Out-File -FilePath .\temp.csv
+    Import-Csv -Path .\temp.csv -Header $Header | ConvertTo-Json -Compress | Out-File $N
+    del .\temp.csv
 }
 
 Function CsvToHtml ($File) {
@@ -27,10 +33,10 @@ TD {border-width: 1px; padding: 3px; border-style: solid; border-color: black;}
 </style>
 "@
 
-    $x = $File | Select-Object Directory, BaseName
-    $n = [System.IO.Path]::Combine($x.Directory, (($x.BaseName, 'html') -join "."))
+    $X = $File | Select-Object Directory, BaseName
+    $N = [System.IO.Path]::Combine($X.Directory, (($X.BaseName, 'html') -join "."))
 
-    Import-Csv $File | ConvertTo-Html -Head $Header | Out-File $n
+    Import-Csv $File | ConvertTo-Html -Head $Header | Out-File $N
 }
 
 ########### Execute 
